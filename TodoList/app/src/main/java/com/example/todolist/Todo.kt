4afdf7b88f33491b22aa7df8845baae76f6cd938
jiiiiiyoon <wldbs5897@naver.com>
2021-07-26 15:53:45 +0900

    error,,

--- TodoList/app/src/main/java/com/example/todolist/Todo.kt
+++ TodoList/app/src/main/java/com/example/todolist/Todo.kt
@@ -3,7 +3,8 @@ package com.example.todolist
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-class Todo (@PrimaryKey var id: Long = 0,
+class Todo (
+    @PrimaryKey var id: Long = 0,
             var title: String = "",
             var date: Long = 0
 ) : RealmObject(){
